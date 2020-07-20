3e41440d46d8041807a9f87271731b307fd80c89
Marcus Hodge <marcushodge8@gmail.com>
2020-07-20 15:57:49 -0400

    Init commit

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.listen(3000);
